572d40e1471ef7f6c45a347c810734d8cdf4fe74
Gott50 <xxGott50xx@gmail.com>
2018-09-02 09:20:10 +0200

    refactor

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@
 docker-machine create --driver virtualbox manager1
 
 MANAGER_IP=$(docker-machine ip manager1)
-COMPOSE=$(cat ./docker-compose.yml)
 
 docker-machine ssh manager1 "docker swarm init --advertise-addr $MANAGER_IP"
-docker-machine ssh manager1 "echo '$COMPOSE' > docker-compose.yml"
+docker-machine scp docker-compose.yml manager1:
 docker-machine ssh manager1 <<-'END_SSH'
     docker stack deploy --compose-file docker-compose.yml manager1
 END_SSH
