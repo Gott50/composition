f8fc99bfcf38e3555265e47c232965e0e9d9a6eb
Gott50 <xxGott50xx@gmail.com>
2018-09-03 10:53:27 +0200

    save id_rsa in SSH_KEY in .manager.env

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@
 docker-machine create --driver virtualbox manager1
 
 MANAGER_IP=$(docker-machine ip manager1)
+eval "$(docker-machine env manager1)"
+
+id_rsa=$(cat $DOCKER_CERT_PATH/id_rsa)
+SSH_KEY=${id_rsa//
+/**}
+
+mkdir .env
+echo "IP=$MANAGER_IP
+SSH_KEY=$SSH_KEY" > .env/.manager.env
 
 docker-machine ssh manager1 "docker swarm init --advertise-addr $MANAGER_IP"
 docker-machine scp docker-compose.yml manager1:
