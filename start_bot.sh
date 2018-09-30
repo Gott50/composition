7eb3d9c578878a7b91a913eb8f9a1fa6a16abefe
Gott50 <xxGott50xx@gmail.com>
2018-09-30 18:15:23 +0200

    fix: Error response from daemon: Could not attach to network dev_default: rpc error: code = PermissionDenied desc = network dev_default not manually attachable

--- start_bot.sh
+++ start_bot.sh
@@ -5,6 +5,7 @@ echo Parameters: $@
 sudo docker stop /$1
 sudo docker rm /$1
 
-sudo docker run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 -e ENV=$3 -v log_data:/code/logs instagramtools/web
+NODE=$(docker info | grep "Node Address")
+SELENIUM=${NODE/' Node Address: '/''}
 
-sudo docker network connect bots $1
+sudo docker run -d --name $1 -e SELENIUM=$SELENIUM -e INSTA_USER=$1 -e INSTA_PW=$2 -e ENV=$3 -v log_data:/code/logs instagramtools/web
