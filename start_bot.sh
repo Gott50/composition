bda9e08f91ab48c9980c82a744b9a680c6925594
Gott50 <xxGott50xx@gmail.com>
2018-09-30 17:03:01 +0200

    fix: 'http://selenium:4444/wd/hub' not found

--- start_bot.sh
+++ start_bot.sh
@@ -5,5 +5,6 @@ echo Parameters: $@
 sudo docker stop /$1
 sudo docker rm /$1
 
-sudo docker run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 \
--e ENV=$3 -v log_data:/code/logs instagramtools/web
+sudo docker run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 -e ENV=$3 -v log_data:/code/logs instagramtools/web
+
+sudo docker network connect bots $1
