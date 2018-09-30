1642b60c0fa0e10792b0aefa6f2aad453c030fff
Gott50 <xxGott50xx@gmail.com>
2018-09-30 12:41:05 +0200

    add KNOWN_HOSTS to .manager.env

--- setup.sh
+++ setup.sh
@@ -6,12 +6,14 @@ MANAGER_IP=$(docker-machine ip $name)
 eval "$(docker-machine env $name)"
 
 id_rsa=$(cat $DOCKER_CERT_PATH/id_rsa)
+ssh -i $DOCKER_CERT_PATH/id_rsa docker@$MANAGER_IP echo "ssh works!"
 SSH_KEY=${id_rsa//
 /_}
 
 mkdir .env
 echo "IP=$MANAGER_IP
-SSH_KEY=$SSH_KEY" > .env/.manager.env
+SSH_KEY=$SSH_KEY
+KNOWN_HOSTS=$(grep $MANAGER_IP ~/.ssh/known_hosts)" > .env/.manager.env
 
 docker-machine ssh $name "sudo docker swarm init --advertise-addr $MANAGER_IP"
 docker-machine scp docker-compose.yml $name:
