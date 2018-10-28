f500e554725603330062e84e501e3d83c06d9487
Gott50 <xxGott50xx@gmail.com>
2018-10-28 13:23:51 +0100

    use bash for start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo Parameters: $@
 INSTA_USER=$2
@@ -7,7 +7,7 @@ sudo docker stop /$INSTA_USER
 sudo docker rm /$INSTA_USER
 
 NODE=$(sudo docker info | grep "Node Address")
-SELENIUM=${NODE/' Node Address: '/''}
+SELENIUM=${NODE/'Node Address: '/''}
 
 
 sudo docker run -d --name $INSTA_USER -e SELENIUM=$SELENIUM -e ENV=$1 -e INSTA_USER=$INSTA_USER -e INSTA_PW=$3 -e PROXY=$4 -v log_data:/code/logs instagramtools/web
