0291ee8df44ed33074660f14f505d52531a1cb10
Gott50 <xxGott50xx@gmail.com>
2018-11-26 17:34:49 +0100

    use IP

--- start_bot.sh
+++ start_bot.sh
@@ -8,8 +8,11 @@ sudo docker rm /$INSTA_USER
 
 NODE=$(sudo docker info | grep "Node Address")
 SELENIUM=${NODE/' Node Address: '/''}
+# 18.196.202.48
+# should be
+# 18.197.130.112
 
-CMD="sudo docker run -d --name $INSTA_USER -e SELENIUM=$SELENIUM -e ENV=$1 -e INSTA_USER=$INSTA_USER -e INSTA_PW=$3 -e PROXY=$4 -v log_data:/code/logs instagramtools/web"
+CMD="sudo docker run -d --name $INSTA_USER -e SELENIUM=18.197.130.112 -e ENV=$1 -e INSTA_USER=$INSTA_USER -e INSTA_PW=$3 -e PROXY=$4 -v log_data:/code/logs instagramtools/web"
 echo CMD: $CMD
 
 $CMD
