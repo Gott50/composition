e7074086cfbbbd242a6df41b16324c3a504e536b
Gott50 <xxGott50xx@gmail.com>
2018-12-01 18:31:23 +0100

    remove Spaces from SETTINGS

--- start_bot.sh
+++ start_bot.sh
@@ -8,13 +8,15 @@ sudo docker stop /$INSTA_USER
 echo "Composition sudo docker rm /$INSTA_USER"
 sudo docker rm /$INSTA_USER
 
+SETTINGS=${1/' '/''}
+
 NODE=$(sudo docker info | grep "Node Address")
 SELENIUM=${NODE/' Node Address: '/''}
 # 18.196.202.48
 # should be
 # 18.197.130.112
 
-CMD="sudo docker run -d -e SELENIUM=18.197.130.112 --name $INSTA_USER -e ENV='$1' -e INSTA_USER=$INSTA_USER -e INSTA_PW=$3 -e PROXY=$4 -v log_data:/code/logs instagramtools/web"
+CMD="sudo docker run -d -e SELENIUM=18.197.130.112 --name $INSTA_USER -e ENV=$SETTINGS -e INSTA_USER=$INSTA_USER -e INSTA_PW=$3 -e PROXY=$4 -v log_data:/code/logs instagramtools/web"
 echo Composition CMD: $CMD
 
 $CMD
