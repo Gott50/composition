497fc0c805f9763c6dcc61c62842fe4d0efa38ef
Gott50 <xxGott50xx@gmail.com>
2019-03-27 23:01:33 +0100

    add dropTables.sql

---
+++ dropTables.sql
@@ -0,0 +1,6 @@
+drop table if exists "user" cascade;
+drop table if exists "timetable" cascade;
+drop table if exists "running" cascade;
+drop table if exists "roles_users" cascade;
+drop table if exists "role" cascade;
+drop table if exists "account" cascade;
