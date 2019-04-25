993f198a8a2463587cf484810c3bac8c26d4924d
Gott50 <xxGott50xx@gmail.com>
2019-04-26 00:03:24 +0200

    move scripts to /scripts

--- setup.sh
+++ setup.sh
@@ -40,8 +40,7 @@ ENDBACKUP
 docker-machine ssh $name "sudo docker swarm init --advertise-addr $MANAGER_IP"
 docker-machine scp docker-prod.yml $name:
 docker-machine scp -r .env/ $name:
-docker-machine scp start_bot.sh $name:
-docker-machine scp docker_clean.sh $name:
+docker-machine scp scripts $name:
 docker-machine ssh $name "sudo docker stack deploy --compose-file docker-prod.yml $name"
 
 docker-machine scp docker-compose-bot.yml $name:docker-compose.yml
