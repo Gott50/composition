3fd16f259ee582b7a6f7a0414f72a1a8de24e648
Gott50 <xxGott50xx@gmail.com>
2019-05-06 13:25:08 +0200

    update scripts/docker_clean.sh

--- scripts/docker_clean.sh
+++ scripts/docker_clean.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Delete all containers
-docker rm $(docker ps -a -q)
-# Delete all images
-docker rmi $(docker images -q)
+# remove exited containers:
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+
+# remove unused images:
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+
+# remove unused volumes (needs to be ran as root):
+find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+  docker ps -aq | xargs docker inspect | jq -r '.[]|.Mounts|.[]|.Name|select(.)'
+) | xargs -r rm -fr
