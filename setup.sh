9d6dd50fda37f945d4dcd3491b2b3b97cc91d27d
Gott50 <xxGott50xx@gmail.com>
2019-05-09 23:06:33 +0200

    fix: docker-machine ssh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,6 @@ docker-machine scp docker-prod.yml $name:
 docker-machine scp -r .env/ $name:
 docker-machine scp -r scripts/ $name:
 
-docker-machine ssh $name <<-'ENDSTART'
-    sudo docker swarm init --advertise-addr $MANAGER_IP
-    sudo docker stack deploy --compose-file docker-prod.yml $name
-    sudo sh scripts/docker_clean.sh
-ENDSTART
-
+docker-machine ssh $name "sudo docker swarm init --advertise-addr $MANAGER_IP"
+docker-machine ssh $name "sudo docker stack deploy --compose-file docker-prod.yml $name"
+docker-machine ssh $name "sudo sh scripts/docker_clean.sh"
